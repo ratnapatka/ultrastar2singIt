db4c4cc96113b66a6a5f2da527b821b4dc3cc670
ratnapatka <ratnapatka@gmail.com>
2026-03-15 14:15:49 +0100

    add songs to list, fix 2024 fra

--- data/repository/DlcRepository.py
+++ data/repository/DlcRepository.py
@@ -12,6 +12,7 @@ def _get_connection() -> sqlite3.Connection:
 def init():
     with _get_connection() as conn:
         c = conn.cursor()
+        c.execute("""drop table dlc""")
         c.execute("""CREATE TABLE IF NOT EXISTS dlc (
                 dlc_id TEXT PRIMARY KEY,
                 dlc_name TEXT,
@@ -22,45 +23,78 @@ def init():
 
         c.execute("SELECT COUNT(1) FROM dlc")
         if c.fetchone()[0] == 0:
-            data = (('0100CC30149B9002','Best of 80s Vol 1 Song Pack',None,'0100CC30149B8000','2022'),
-            ('0100CC30149B9007','Best of 80s Vol 2 Song Pack',None,'0100CC30149B8000','2022'),
-            ('0100CC30149B900A','Best of 80s Vol 3 Song Pack',None,'0100CC30149B8000','2022'),
-            ('0100CC30149B9006','Best of 90s Vol 1 Song Pack',None,'0100CC30149B8000','2022'),
-            ('0100CC30149B9008','Best of 90s Vol 2 Song Pack',None,'0100CC30149B8000','2022'),
-            ('0100CC30149B9011','Best of 90s Vol 3 Song Pack',None,'0100CC30149B8000','2022'),
-            ('0100CC30149B9003','Chart Hits Song Pack',None,'0100CC30149B8000','2022'),
-            ('0100CC30149B900B','Classic Rock Song Pack',None,'0100CC30149B8000','2022'),
-            ('0100CC30149B9009','Country Hits Song Pack',None,'0100CC30149B8000','2022'),
-            ('0100CC30149B900D','French Song Pack',None,'0100CC30149B8000','2022'),
-            ('0100CC30149B900E','German Song Pack',None,'0100CC30149B8000','2022'),
-            ('0100CC30149B900C','International Songs',None,'0100CC30149B8000','2022'),
-            ('0100CC30149B9005','Legendary Hits Vol 1 Song Pack',None,'0100CC30149B8000','2022'),
-            ('0100CC30149B9010','Legendary Hits Vol 2 Song Pack',None,'0100CC30149B8000','2022'),
-            ('0100CC30149B9001','Party Classics Vol 1 Song Pack',None,'0100CC30149B8000','2022'),
-            ('0100CC30149B9004','Party Classics Vol 2 Song Pack',None,'0100CC30149B8000','2022'),
-            ('0100CC30149B900F','Spanish Song Pack',None,'0100CC30149B8000','2022'),
-            ('0100D7701692F002','Best of 80s Vol. 1 Song Pack',None,'0100D7701692E000','2023'),
-            ('0100D7701692F007','Best of 80s Vol. 2 Song Pack',None,'0100D7701692E000','2023'),
-            ('0100D7701692F00A','Best of 80s Vol. 3 Song Pack',None,'0100D7701692E000','2023'),
-            ('0100D7701692F006','Best of 90s Vol. 1 Song Pack',None,'0100D7701692E000','2023'),
-            ('0100D7701692F008','Best of 90s Vol. 2 Song Pack',None,'0100D7701692E000','2023'),
-            ('0100D7701692F011','Best Of 90s Vol. 3 Song Pack',None,'0100D7701692E000','2023'),
-            ('0100D7701692F003','Chart Hits Song Pack',None,'0100D7701692E000','2023'),
-            ('0100D7701692F013','Christmas Hits Song Pack',None,'0100D7701692E000','2023'),
-            ('0100D7701692F00B','Classic Rock Song Pack',None,'0100D7701692E000','2023'),
-            ('0100D7701692F009','Country Hits Song Pack',None,'0100D7701692E000','2023'),
-            ('0100D7701692F014','Eurovision Hits Song Pack',None,'0100D7701692E000','2023'),
-            ('0100D7701692F00C','International Song Pack ',None,'0100D7701692E000','2023'),
-            ('0100D7701692F005','Legendary Hits Vol. 1 Song Pack',None,'0100D7701692E000','2023'),
-            ('0100D7701692F010','Legendary Hits Vol. 2 Song Pack',None,'0100D7701692E000','2023'),
-            ('0100D7701692F001','Party Classics Vol. 1 Song Pack',None,'0100D7701692E000','2023'),
-            ('0100D7701692F004','Party Classics Vol. 2 Song Pack',None,'0100D7701692E000','2023'),
-            ('0100AAF018059005','Canciones Espanolas','songs_spa','0100AAF018058000','2024'),
-            ('0100AAF018059003','Deutschen Hits','songs_ger','0100AAF018058000','2024'),
-            ('0100AAF018059002','Hits Francais et Internationaux','songs_fr','0100AAF018058000','2024'),
-            ('0100AAF018059001','Hits from Australia and NZ','songs_aus','0100AAF018058000','2024'),
-            ('0100AAF018059004','International Hits','songs_int','0100AAF018058000','2024'),
-            ('01001C101ED11002','French Hits','songs_fr','01001C101ED10000','2025'))
+            data = (
+                ('0100382004207002','DLC Best of 80s Song Pack',None,'0100382004206000','2018'),
+                ('0100382004207003','DLC Chart Hits Song Pack',None,'0100382004206000','2018'),
+                ('0100382004207005','DLC Legendary Hits Song Pack',None,'0100382004206000','2018'),
+                ('0100382004207001','DLC Party Classics Song Pack',None,'0100382004206000','2018'),
+                ('0100382004207004','DLC Party Classics Vol 2 Song Pack',None,'0100382004206000','2018'),
+                ('0100953009DF9002','DLC Best of 80s Vol 1 Song Pack',None,'0100953009DF8000','2019'),
+                ('0100953009DF9007','DLC Best of 80s Vol 2 Song Pack',None,'0100953009DF8000','2019'),
+                ('0100953009DF9006','DLC Best of 90s Song Pack',None,'0100953009DF8000','2019'),
+                ('0100953009DF9003','DLC Chart Hits Song Pack',None,'0100953009DF8000','2019'),
+                ('0100953009DF9005','DLC Legendary Hits Song Pack',None,'0100953009DF8000','2019'),
+                ('0100953009DF9001','DLC Party Classics Vol 1 Song Pack',None,'0100953009DF8000','2019'),
+                ('0100953009DF9004','DLC Party Classics Vol 2 Song Pack',None,'0100953009DF8000','2019'),
+                ('010065F00DF4B007','DLC Best of 80s Vol 2 Song Pack',None,'010065F00DF4A000','2020'),
+                ('010065F00DF4B002','DLC Best of 80s Vol.1 Song Pack',None,'010065F00DF4A000','2020'),
+                ('010065F00DF4B006','DLC Best of 90s Vol 1 Song Pack',None,'010065F00DF4A000','2020'),
+                ('010065F00DF4B008','DLC Best of 90s Vol 2 Song Pack',None,'010065F00DF4A000','2020'),
+                ('010065F00DF4B003','DLC Chart Hits Song Pack',None,'010065F00DF4A000','2020'),
+                ('010065F00DF4B009','DLC Country Hits Song Pack',None,'010065F00DF4A000','2020'),
+                ('010065F00DF4B005','DLC Legendary Hits Song Pack',None,'010065F00DF4A000','2020'),
+                ('010065F00DF4B001','DLC Party Classics Vol 1 Song Pack',None,'010065F00DF4A000','2020'),
+                ('010065F00DF4B004','DLC Party Classics Vol 2 Song Pack',None,'010065F00DF4A000','2020'),
+                ('010061F0119FB002','DLC Best of 80s Vol 1 Song Pack',None,'010061F0119FA000','2021'),
+                ('010061F0119FB007','DLC Best of 80s Vol 2 Song Pack',None,'010061F0119FA000','2021'),
+                ('010061F0119FB00A','DLC Best of 80s Vol 3 Song Pack',None,'010061F0119FA000','2021'),
+                ('010061F0119FB006','DLC Best of 90s Vol 1 Song Pack',None,'010061F0119FA000','2021'),
+                ('010061F0119FB008','DLC Best of 90s Vol 2 Song Pack',None,'010061F0119FA000','2021'),
+                ('010061F0119FB003','DLC Chart Hits Song Pack',None,'010061F0119FA000','2021'),
+                ('010061F0119FB00B','DLC Classic Rock Song Pack',None,'010061F0119FA000','2021'),
+                ('010061F0119FB009','DLC Country Hits Song Pack',None,'010061F0119FA000','2021'),
+                ('010061F0119FB005','DLC Legendary Hits Song Pack',None,'010061F0119FA000','2021'),
+                ('010061F0119FB001','DLC Party Classics Vol 1 Song Pack',None,'010061F0119FA000','2021'),
+                ('010061F0119FB004','DLC Party Classics Vol 2 Song Pack',None,'010061F0119FA000','2021'),
+                ('0100CC30149B9002','Best of 80s Vol 1 Song Pack',None,'0100CC30149B8000','2022'),
+                ('0100CC30149B9007','Best of 80s Vol 2 Song Pack',None,'0100CC30149B8000','2022'),
+                ('0100CC30149B900A','Best of 80s Vol 3 Song Pack',None,'0100CC30149B8000','2022'),
+                ('0100CC30149B9006','Best of 90s Vol 1 Song Pack',None,'0100CC30149B8000','2022'),
+                ('0100CC30149B9008','Best of 90s Vol 2 Song Pack',None,'0100CC30149B8000','2022'),
+                ('0100CC30149B9011','Best of 90s Vol 3 Song Pack',None,'0100CC30149B8000','2022'),
+                ('0100CC30149B9003','Chart Hits Song Pack',None,'0100CC30149B8000','2022'),
+                ('0100CC30149B900B','Classic Rock Song Pack',None,'0100CC30149B8000','2022'),
+                ('0100CC30149B9009','Country Hits Song Pack',None,'0100CC30149B8000','2022'),
+                ('0100CC30149B900D','French Song Pack',None,'0100CC30149B8000','2022'),
+                ('0100CC30149B900E','German Song Pack',None,'0100CC30149B8000','2022'),
+                ('0100CC30149B900C','International Songs',None,'0100CC30149B8000','2022'),
+                ('0100CC30149B9005','Legendary Hits Vol 1 Song Pack',None,'0100CC30149B8000','2022'),
+                ('0100CC30149B9010','Legendary Hits Vol 2 Song Pack',None,'0100CC30149B8000','2022'),
+                ('0100CC30149B9001','Party Classics Vol 1 Song Pack',None,'0100CC30149B8000','2022'),
+                ('0100CC30149B9004','Party Classics Vol 2 Song Pack',None,'0100CC30149B8000','2022'),
+                ('0100CC30149B900F','Spanish Song Pack',None,'0100CC30149B8000','2022'),
+                ('0100D7701692F002','Best of 80s Vol. 1 Song Pack',None,'0100D7701692E000','2023'),
+                ('0100D7701692F007','Best of 80s Vol. 2 Song Pack',None,'0100D7701692E000','2023'),
+                ('0100D7701692F00A','Best of 80s Vol. 3 Song Pack',None,'0100D7701692E000','2023'),
+                ('0100D7701692F006','Best of 90s Vol. 1 Song Pack',None,'0100D7701692E000','2023'),
+                ('0100D7701692F008','Best of 90s Vol. 2 Song Pack',None,'0100D7701692E000','2023'),
+                ('0100D7701692F011','Best Of 90s Vol. 3 Song Pack',None,'0100D7701692E000','2023'),
+                ('0100D7701692F003','Chart Hits Song Pack',None,'0100D7701692E000','2023'),
+                ('0100D7701692F013','Christmas Hits Song Pack',None,'0100D7701692E000','2023'),
+                ('0100D7701692F00B','Classic Rock Song Pack',None,'0100D7701692E000','2023'),
+                ('0100D7701692F009','Country Hits Song Pack',None,'0100D7701692E000','2023'),
+                ('0100D7701692F014','Eurovision Hits Song Pack',None,'0100D7701692E000','2023'),
+                ('0100D7701692F00C','International Song Pack ',None,'0100D7701692E000','2023'),
+                ('0100D7701692F005','Legendary Hits Vol. 1 Song Pack',None,'0100D7701692E000','2023'),
+                ('0100D7701692F010','Legendary Hits Vol. 2 Song Pack',None,'0100D7701692E000','2023'),
+                ('0100D7701692F001','Party Classics Vol. 1 Song Pack',None,'0100D7701692E000','2023'),
+                ('0100D7701692F004','Party Classics Vol. 2 Song Pack',None,'0100D7701692E000','2023'),
+                ('0100AAF018059005','Canciones Espanolas','songs_spa','0100AAF018058000','2024'),
+                ('0100AAF018059003','Deutschen Hits','songs_ger','0100AAF018058000','2024'),
+                ('0100AAF018059002','Hits Francais et Internationaux','songs_fra','0100AAF018058000','2024'),
+                ('0100AAF018059001','Hits from Australia and NZ','songs_aus','0100AAF018058000','2024'),
+                ('0100AAF018059004','International Hits','songs_int','0100AAF018058000','2024'),
+                ('01001C101ED11002','French Hits','songs_fr','01001C101ED10000','2025'))
             c.executemany("insert into dlc values (?,?,?,?,?)", data)
 
 def get_by_dlc_id(dlc_id: str) -> Optional[DlcEntity]:
@@ -98,9 +132,5 @@ def get_core_editions() -> List[DlcEntity]:
         rows = c.fetchall()
         return [DlcEntity(core_id=row[1], core_edition=row[0]) for row in rows]
 
-# print(get_by_dlc_id('0100CC30149B9009'))
-# list = get_by_core_edition('2024')
-# filtered = [e for e in list if e.dlc_json_name == 'songs_spa']
-# print(filtered)
-# list = get_all()
-# print(sorted({e.core_edition for e in list}))
+if __name__ == '__main__':
+    init()
